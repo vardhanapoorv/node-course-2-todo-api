484cc528bc488e5ba64d0c968e0808d0626ec488
Apoorv Vardhan <vardhanapoorv@gmail.com>
2017-10-07 20:55:30 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//mongoose.connect( db.localhost || db.mlab);
+mongoose.connect('mongodb://vardhanapoorv:vgsamay16@ds013475.mlab.com:13475/apoorv' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
