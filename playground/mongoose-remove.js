ebf64504ba41f9261d1466cf68fb9f40b7820f3f
Apoorv Vardhan <vardhanapoorv@gmail.com>
2017-10-08 13:35:44 +0530

    Add tests for PATCH /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -12,6 +12,10 @@ const {User} =require('./../server/models/user');
 // Todo.findOneAndRemove
 // Todo.findByIdAndRemove
 
+Todo.findOneAndRemove({_id: '59d9a44a7d80750687132797'}).then((todo)) => {
+  
+}
+
 Todo.findByIdAndRemove('59d9a44a7d80750687132797').then((todo) => {
   console.log(todo);
 })
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //mongoose.connect( db.localhost || db.mlab);
-mongoose.connect('mongodb://vardhanapoorv:vgsamay16@ds013475.mlab.com:13475/apoorv' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://vardhanapoorv:vgsamay16@ds013475.mlab.com:13475/apoorv');
 
 module.exports = {mongoose};
