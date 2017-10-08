7402d8790be8c30fe30909a4fec0e46cc24e970e
Apoorv Vardhan <vardhanapoorv@gmail.com>
2017-10-08 10:20:44 +0530

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} =require('./../server/models/user');
+
+// Todo.remove
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('59d9a44a7d80750687132797').then((todo) => {
+  console.log(todo);
+})
