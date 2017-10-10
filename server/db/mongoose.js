3566dc87f8f9fbcec78f6ae05da0531baf893783
Apoorv Vardhan <vardhanapoorv@gmail.com>
2017-10-10 18:26:29 +0530

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //mongoose.connect( db.localhost || db.mlab);
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://vardhanapoorv:vgsamay16@ds013475.mlab.com:13475/apoorv');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
