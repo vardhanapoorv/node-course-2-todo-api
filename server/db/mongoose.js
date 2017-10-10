7a3ca2327b6f9d135fa2f40801c6aef07f4d28f5
Apoorv Vardhan <vardhanapoorv@gmail.com>
2017-10-10 20:33:49 +0530

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect( db.localhost || db.mlab);
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -89,6 +90,21 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
+
+// POST /users
+app.post('/users',(req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
